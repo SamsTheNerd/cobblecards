fe40ef12a237d0bfcf59bf71c31a4214c909edec
SamsTheNerd <sam@samsthenerd.com>
2024-02-05 20:55:49 -0500

    a little bit of cleanup of redundant classes - mostly tooltips that got merged into WHTexture tooltips

--- common/src/main/java/com/samsthenerd/cobblecards/registry/CobbleCardsTooltips.java
+++ common/src/main/java/com/samsthenerd/cobblecards/registry/CobbleCardsTooltips.java
@@ -4,11 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
-import com.samsthenerd.cobblecards.tooltips.components.PokemonCardTooltipComponent;
-import com.samsthenerd.cobblecards.tooltips.components.URLImageTooltipComponent;
 import com.samsthenerd.cobblecards.tooltips.components.WHTextureTooltipComponent;
-import com.samsthenerd.cobblecards.tooltips.data.PokemonCardTooltipData;
-import com.samsthenerd.cobblecards.tooltips.data.URLImageTooltipData;
 import com.samsthenerd.cobblecards.tooltips.data.WHTextureTooltipData;
 
 import net.minecraft.client.gui.tooltip.TooltipComponent;
@@ -35,8 +31,6 @@ public class CobbleCardsTooltips {
 
     // should be called sided in tooltip registration stuff
     public static void init(){
-        tooltipDataToComponent.put(PokemonCardTooltipData.class, convertTooltip(PokemonCardTooltipData.class, PokemonCardTooltipComponent::new));
-        tooltipDataToComponent.put(URLImageTooltipData.class, convertTooltip(URLImageTooltipData.class, URLImageTooltipComponent::new));
         tooltipDataToComponent.put(WHTextureTooltipData.class, convertTooltip(WHTextureTooltipData.class, WHTextureTooltipComponent::new));
     }
 }
